ed74e78b3a65889721dfdd1751364702a0a8a77c
Joe Bowen <Joe.Bowen@sjrb.ca>
2019-03-11 08:24:01 -0600

    Updates

--- libraries/compound.py
+++
@@ -1,9 +0,0 @@
-import math
-
-import libraries.constants as constants
-
-from libraries.thermochemical import BaseThermoChemical
-
-class CompoundThermoChemical(BaseThermoChemical):
-    def __init__(compound_name, a_array,  ):
-        pass
